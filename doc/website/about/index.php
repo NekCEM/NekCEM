697eff7a27b31c8eeb2a86f9fdb9ae2940312b43
gcobb <gcobb@df63514b-fc50-0410-8a93-94e60f5a2dad>
2008-07-22 14:58:17 +0000

    added the website to source control
    git-svn-id: https://svn.mcs.anl.gov/repos/NEKCEM/trunk@19 df63514b-fc50-0410-8a93-94e60f5a2dad

---
+++ doc/website/about/index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; utf-8" />
+	<title>NEKCEM: Nekton for Computational ElectroMagnetics</title>
+	<link href="../css/screen.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body id="about">
+<div id="container">
+	<div id="mastheadwrap">
+		<div id="header">
+            <img src="../images/header_nekcem.jpg" alt="header" width="850" height="86" />
+			<img src="../images/nekcem_logo.png" alt="NEKCEM logo" width="850" height="90" />
+	
+		  <div id="nav">
+			<?php include('../inc/navbar.php'); ?>
+		  </div>
+	  </div>
+  </div>
+	
+	<!-- content -->
+	<div id="content">
+	
+		<!-- rightside column -->
+		<div id="right">
+			
+			   <h3>View Examples </h3>
+				<div class='featurebox_side'>
+                put a screen shot here.
+				<!-- <img src="../images/about/rd100.gif" alt="R &amp; D 100" width="120" height="128" /> -->
+				</div>
+				<p>Here are a few excellent examples of NEKCEM. Blah, blah, blah. And a littlte more blah, blah.....</p>
+				</div>	
+	
+		<!-- end right column-->
+		<!-- main content area-->
+		<div id="center">
+			
+				<h1>About Nekcem</h1>
+                <p>
+                In the software package  NEKCEM  (Nekton for Computational ElectroMagnetics) the spectral element discontinuous 
+                Galerkin  (SEDG)  method is employed on a conformal spectral-element mesh. NEKCEM features geometric flexibility, 
+                spectrally accurate numerical convergence, and an efficient parallel implementation.
+                </p>
+                <p>
+                This package consists of the components of meshing, Maxwell time domain solver, electrostatic poisson solver, 
+                and visualization tools. It supports the computational domain boundary with perfectly matched layer, perfect 
+                conductor, and periodic material.
+                </p>
+                <h2>Collaborators</h2>
+                <p>Misun Min</p>
+                <p>Paul Fischer</p>
+
+		</div>
+		<p>
+		  <!-- end main content area-->
+	  </p>
+		<p>&nbsp;</p>
+		<p>&nbsp;</p>
+		<p><br class="clear" />
+	            </p>
+		<div id="footer">
+	<?php include('../inc/footer.php'); ?>
+	</div>
+	</div>
+	<!-- end content -->
+	
+	
+    <br class="clear" />
+	
+</div>
+</body>
+</html>
