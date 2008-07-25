874e3da621506daf72c9fb56ff9063625b93e58d
gcobb <gcobb@df63514b-fc50-0410-8a93-94e60f5a2dad>
2008-07-25 19:12:41 +0000

    deleted the website directory, since it's now under /www
    git-svn-id: https://svn.mcs.anl.gov/repos/NEKCEM/trunk@29 df63514b-fc50-0410-8a93-94e60f5a2dad

--- doc/website/inc/navbar.php
+++
@@ -1,6 +0,0 @@
-<ul>
-    <li><a href="http://www.mcs.anl.gov/research/projects/nekcem/index.php" class="home">Home</a></li>
-    <li><a href="http://www.mcs.anl.gov/research/projects/nekcem/about/index.php" class="about">About NEKCEM</a></li>
-    <li><a href="http://www.mcs.anl.gov/research/projects/nekcem/docs/index.php" class="docs">Documentation</a></li>
-    <li><a href="http://www.mcs.anl.gov/research/projects/nekcem/downloads/index.php" class="downloads">Downloads</a></li>
-</ul>
