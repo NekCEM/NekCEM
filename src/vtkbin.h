18659738a0e2fbb7e1046eb0ece69c352485799f
jingfu <jingfu@df63514b-fc50-0410-8a93-94e60f5a2dad>
2010-07-13 22:00:10 +0000

    updated rbIO_netcem.c (for N-M-1), to be tested, non-significant check-in..
    git-svn-id: https://svn.mcs.anl.gov/repos/NEKCEM/trunk@446 df63514b-fc50-0410-8a93-94e60f5a2dad

--- src/vtkbin.h
+++ src/vtkbin.h
@@ -1,5 +1,9 @@
 #ifndef H_VTKBIN
 #define H_VTKBIN
+char filename[100];
+char mFilename[100];
+char rbFilename[100];
+
 
 void getfieldname_( int i, char *name );
 void getfilename_(int *id, int *nid );
