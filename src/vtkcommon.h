2e5e4e0266515fa293112ad00fa0a96cee6d6613
jingfu <jingfu@df63514b-fc50-0410-8a93-94e60f5a2dad>
2010-07-26 02:00:31 +0000

    working version of N1 and NM1 ascii, NM1 binary part need to be modified back to int (instead of long long)
    git-svn-id: https://svn.mcs.anl.gov/repos/NEKCEM/trunk@482 df63514b-fc50-0410-8a93-94e60f5a2dad

--- src/vtkcommon.h
+++ src/vtkcommon.h
@@ -1,7 +1,10 @@
 #ifndef H_VTKBIN
 #define H_VTKBIN
+#include "rdtsc.h"
 
 #define ONE_MILLION 1048576
+#define BGP_FREQ (850*1000000)
+#define V8_FREQ (3000*1000000)
 
 extern char filename[100];
 extern char mFilename[100];
@@ -16,12 +19,16 @@ extern long long fieldSizeSum ;
 extern int myrank, mysize;
 extern int DEBUG_FLAG;
 
+extern long long start_time;
+extern long long end_time;
+extern double overall_time;
+
 void getfieldname_( int i, char *name );
 void getfilename_(int *id, int *nid );
 void adjust_endian();
 int swap_int_byte(int *n);
 int swap_float_byte(float *n);
-
+int swap_long_long_byte(long long *n);
 
 
 #endif
