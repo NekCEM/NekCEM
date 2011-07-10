a93b4ad1e9b9551d44995ca81de4206cf3bbed76
misunmin <mmin@mcs.anl.gov>
2011-07-10 19:48:50 +0000

    bugs all fixed for restarting fields in double
    git-svn-id: https://svn.mcs.anl.gov/repos/NEKCEM/trunk@621 df63514b-fc50-0410-8a93-94e60f5a2dad

--- src/io_driver.c
+++ src/io_driver.c
@@ -15,45 +15,49 @@
 #endif
 #endif
 
-#define cem_out_fields FORTRAN_NAME (cem_out_fields , CEM_OUT_FIELDS )
+#define cem_restart_out FORTRAN_NAME (cem_restart_out, CEM_RESTART_OUT)
+#define cem_out_fields  FORTRAN_NAME (cem_out_fields , CEM_OUT_FIELDS )
 #define cem_out_fields2 FORTRAN_NAME(cem_out_fields2, CEM_OUT_FIELDS2)
 #define cem_out_fields3 FORTRAN_NAME(cem_out_fields3, CEM_OUT_FIELDS3)
 #define cem_out_fields4 FORTRAN_NAME(cem_out_fields4, CEM_OUT_FIELDS4)
 #define cem_out_fields6 FORTRAN_NAME(cem_out_fields6, CEM_OUT_FIELDS6)
 
 #ifdef UPCASE
-void CHECKPOINT_WRITE (int* ioop) 
+void CHECKPOINT_WRITE (int* ioop, int* idump)
 #elif IBM
-void checkpoint_write (int* ioop) 
+void checkpoint_write (int* ioop, int* idump)
 #else
-void checkpoint_write_(int* ioop) 
+void checkpoint_write_(int* ioop, int* idump)
 #endif
 {
 	if (*ioop == 0) {
-		cem_out_fields4();
-		cem_out_fields3();
-		cem_out_fields2();
+		cem_out_fields4(idump);
+		cem_out_fields3(idump);
+		cem_out_fields2(idump);
 	}
 	else if (*ioop == 1) {
-		cem_out_fields();
+		cem_out_fields(idump);
 	}
 	else if (*ioop == 2) {
-		cem_out_fields2();
+		cem_out_fields2(idump);
 	}
 	else if (*ioop == 3) {
-		cem_out_fields3();
+		cem_out_fields3(idump);
 	}
 	else {
 #ifdef MPI
-		if (*ioop == 4) {
-			cem_out_fields4();
+		if      (*ioop == 99) {
+			cem_restart_out(idump);
+		}
+		else if (*ioop == 4) {
+			cem_out_fields4(idump);
 		}
 		else if (*ioop == 5) {
 			set_ascii_nm();
-			cem_out_fields6();
+			cem_out_fields6(idump);
 		}
 		else if (*ioop == 6) {
-			cem_out_fields6();
+			cem_out_fields6(idump);
 		}
 		else if (*ioop == -6) {
 			set_ascii_true();
@@ -61,7 +65,7 @@ void checkpoint_write_(int* ioop)
 		}
 		else if (*ioop == 8) {
 			set_ascii_nmm();
-			cem_out_fields6();
+			cem_out_fields6(idump);
 		}
 		else {
 			printf("ERROR: unknown io_option %d\n", *ioop);
