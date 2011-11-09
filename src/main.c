3a6132867a6ec79c98912758c542979d0fedc902
misunmin <mmin@mcs.anl.gov>
2011-11-09 20:14:56 +0000

    BGP test done related to dclock in new setting; ftime is not allowed to use
    git-svn-id: https://svn.mcs.anl.gov/repos/NEKCEM/trunk@639 df63514b-fc50-0410-8a93-94e60f5a2dad

--- src/main.c
+++ src/main.c
@@ -1,17 +1,18 @@
-#ifndef NO_C_MAIN
-#ifdef IBM
-extern void drive_nekton();
-int main()
-{
-  drive_nekton();
-  return 0;
-}
-#else
-extern void drive_nekton_();
-int main()
-{
-  drive_nekton_();
-  return 0;
-}
-#endif
-#endif
+/*#ifndef NO_C_MAIN
+  #ifdef IBM
+  extern void drive_nekton();
+  int main()
+  {
+    drive_nekton();
+    return 0;
+  }
+  #else
+  extern void drive_nekton_();
+  int main()
+  {
+    drive_nekton_();
+    return 0;
+  }
+  #endif
+  #endif
+*/
