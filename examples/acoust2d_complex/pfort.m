038cfd9826cc27e01cc477283813656e6151c381
misunmin <mmin@mcs.anl.gov>
2013-06-05 23:49:28 +0000

    pfort.m: improved figure form
    git-svn-id: https://svn.mcs.anl.gov/repos/NEKCEM/trunk@1104 df63514b-fc50-0410-8a93-94e60f5a2dad

--- examples/acoust2d_complex/pfort.m
+++ examples/acoust2d_complex/pfort.m
@@ -24,13 +24,6 @@ maxmin1=[max(u ), min(u)]
 maxmin2=[max(ur), min(ur)]
 maxmin3=[max(ui), min(ui)]
 
-maxerr_real=max(u-ur)
-maxerr_imag=max(u-ui)
-
-err_real= (uu-uur);
-err_imag= (uu-uui);
-
-
 tmax1=maxmin1(1);
 tmax2=maxmin1(2);
 tmax3=maxmin2(1);
@@ -38,24 +31,36 @@ tmax4=maxmin2(2);
 tmax5=maxmin3(1);
 tmax6=maxmin3(2);
 
+err_real= (uu-uur);
+err_imag= (uu-uui);
+
+errmax1= max(max(uu-uur))
+errmin1= min(min(uu-uur))
+errmax2= max(max(uu-uui))
+errmin2= min(min(uu-uui))
+
+xmax=max(max(xx)); xmin=min(min(xx));
+ymax=max(max(yy)); ymin=min(min(yy));
 
 %figure(1);
 % subplot(3,1,1);plot3(x,y,u );caxis('auto'); xlabel('input'); 
 % subplot(3,1,2);plot3(x,y,ur);caxis('auto'); xlabel('interp: real');
 % subplot(3,1,3);plot3(x,y,ui);caxis('auto'); xlabel('interp: imag');
+
  figure(2); title('comparison: interpolations'); 
- subplot(3,1,1);mesh(xx,yy,uu ); xlabel(['input: max=',num2str(tmax1),' min=',num2str(tmax2)]); 
- view(2); axis([0 2*pi -0.1 1]); colorbar; 
- subplot(3,1,2);mesh(xx,yy,uur); xlabel(['input: max=',num2str(tmax3),' min=',num2str(tmax4)]); view(2) 
- view(2); axis([0 2*pi -0.1 1]); colorbar; 
- subplot(3,1,3);mesh(xx,yy,uui); xlabel(['input: max=',num2str(tmax5),' min=',num2str(tmax6)]); view(2) 
- view(2); axis([0 2*pi -0.1 1]); colorbar; 
- figure(3);
- subplot(3,1,1);mesh(xx,yy,err_imag); xlabel(['input: max=',num2str(tmax1),' min=',num2str(tmax2)]); view(2) 
- view(2); axis([0 2*pi -0.1 1]); colorbar; 
- subplot(3,1,2);mesh(xx,yy,err_imag); xlabel(['input: max=',num2str(tmax3),' min=',num2str(tmax4)]); view(2) 
- view(2); axis([0 2*pi -0.1 1]); colorbar; 
- subplot(3,1,3);mesh(xx,yy,err_imag); xlabel(['input: max=',num2str(tmax3),' min=',num2str(tmax4)]); view(2) 
- view(2); axis([0 2*pi -0.1 1]); colorbar; 
+ subplot(3,1,1);mesh(xx,yy,uu ); xlabel(['imag   input: max=',num2str(tmax1),'; min=',num2str(tmax2)]); 
+ view(2); axis([xmin xmax ymin ymax]); colorbar; ylabel('y') 
+ subplot(3,1,2);mesh(xx,yy,uui); xlabel(['imag numeric: max=',num2str(tmax5),'; min=',num2str(tmax6)]); 
+ view(2); axis([xmin xmax ymin ymax]); colorbar; ylabel('y') 
+ subplot(3,1,3);mesh(xx,yy,uur); xlabel(['real numeric: max=',num2str(tmax3),'; min=',num2str(tmax4)]); 
+ view(2); axis([xmin xmax ymin ymax]); colorbar; ylabel('y') 
+
+ figure(3); title('Error Distributions: all are imaginary part');
+ subplot(3,1,1);mesh(xx,yy,err_imag); xlabel(['err imag: max=',num2str(errmax2),'; min=',num2str(errmin2)]); 
+ view(2); axis([xmin xmax ymin ymax]); colorbar; ylabel('y') 
+ subplot(3,1,2);mesh(xx,yy,err_imag); xlabel(['err imag: max=',num2str(errmax2),'; min=',num2str(errmin2)]); 
+ view(2); axis([xmin xmax ymin ymax]); colorbar; ylabel('y') 
+ subplot(3,1,3);mesh(xx,yy,err_imag); xlabel(['err imag: max=',num2str(errmax2),'; min=',num2str(errmin2)]); 
+ view(2); axis([xmin xmax ymin ymax]); colorbar; ylabel('y') 
 
 
