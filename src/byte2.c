c095b2ed840ca05fd32de254771ce2ab0d7197ac
misunmin <mmin@mcs.anl.gov>
2014-06-06 21:40:57 +0000

    genbox mesh: rea ascii tested; re2 binary to be fixed
    git-svn-id: https://svn.mcs.anl.gov/repos/NEKCEM/trunk@1493 df63514b-fc50-0410-8a93-94e60f5a2dad

--- src/byte2.c
+++ src/byte2.c
@@ -30,12 +30,13 @@
 
 #endif
 
-#define byte_reverse FORTRAN_NAME(byte_reverse, BYTE_REVERSE)
-#define byte_open    FORTRAN_NAME(byte_open,    BYTE_OPEN   )
-#define byte_close   FORTRAN_NAME(byte_close,   BYTE_CLOSE  )
-#define byte_rewind  FORTRAN_NAME(byte_rewind,  BYTE_REWIND )
-#define byte_read    FORTRAN_NAME(byte_read,    BYTE_READ   )
-#define byte_write   FORTRAN_NAME(byte_write,   BYTE_WRITE  )
+#define byte_reverse  FORTRAN_NAME(byte_reverse,  BYTE_REVERSE)
+#define byte_reverse8 FORTRAN_NAME(byte_reverse8, BYTE_REVERSE8)
+#define byte_open     FORTRAN_NAME(byte_open,     BYTE_OPEN   )
+#define byte_close    FORTRAN_NAME(byte_close,    BYTE_CLOSE  )
+#define byte_rewind   FORTRAN_NAME(byte_rewind,   BYTE_REWIND )
+#define byte_read     FORTRAN_NAME(byte_read,     BYTE_READ   )
+#define byte_write    FORTRAN_NAME(byte_write,    BYTE_WRITE  )
 #define set_bytesw_write FORTRAN_NAME(set_bytesw_write,SET_BYTESW_WRITE)
 #define set_bytesw_read  FORTRAN_NAME(set_bytesw_read ,SET_BYTESW_READ )
 #define get_bytesw_write FORTRAN_NAME(get_bytesw_write,GET_BYTESW_WRITE)
@@ -62,16 +63,7 @@ int bytesw_read=0;
   void exitt();
 #endif
 
-void cexitt()
-{
-#ifdef UNDERSCORE
-  exitt_();
-#else
-  exitt();
-#endif
-}
-
-void byte_reverse(float *buf, int *nn)
+void byte_reverse(float *buf, int *nn,int *ierr)
 {
   int n;
   char temp, *ptr;
@@ -79,7 +71,8 @@ void byte_reverse(float *buf, int *nn)
   if (*nn<0)
   {
     printf("byte_reverse() :: n must be positive\n"); 
-    cexitt();
+    *ierr=1;
+    return;
   }
   
   for (ptr=(char *)buf,n=*nn; n--; ptr+=4)
@@ -87,10 +80,39 @@ void byte_reverse(float *buf, int *nn)
      SWAP(ptr[0],ptr[3])
      SWAP(ptr[1],ptr[2])
   }
+  *ierr=0;
+}
+
+void byte_reverse8(float *buf, int *nn,int *ierr)
+{
+  int n;
+  char temp, *ptr;
+
+  if (*nn<0)
+  {
+    printf("byte_reverse8() :: n must be positive\n");
+    *ierr=1;
+    return;
+  }
+  if(*nn % 2 != 0)
+  {
+    printf("byte_reverse8() :: n must be multiple of 2\n");
+    *ierr=1;
+    return;
+  }
+
+  for (ptr=(char *)buf,n=*nn,n=n+2; n-=2; ptr+=8)
+  {
+     SWAP(ptr[0],ptr[7])
+     SWAP(ptr[1],ptr[6])
+     SWAP(ptr[2],ptr[5])
+     SWAP(ptr[3],ptr[4])
+  }
+  *ierr=0;
 }
 
 
-void byte_open(char *n)
+void byte_open(char *n,int *ierr)
 {
   int  i,len,istat;
   char slash;
@@ -101,16 +123,17 @@ void byte_open(char *n)
   if (len<0)
   {
     printf("byte_open() :: file name has negative length!\n"); 
-    cexitt();
+    *ierr=1;
+    return;
   }
 
   if (len>MAX_NAME)
   {
     printf("byte_open() :: file name too long!\n"); 
-    cexitt();
+    *ierr=1;
+    return;
   }
 
-	printf("n string is: %s\n", n);
   strcpy(name,n);
   strcpy(dirname,n);
 
@@ -123,19 +146,22 @@ void byte_open(char *n)
        istat = mkdir(dirname,0755);
      }
   }
+  *ierr=0;
 }
 
-void byte_close()
+void byte_close(int *ierr)
 {
   if (!fp) return;
 
   if (fclose(fp))
   {
     printf("byte_close() :: couldn't fclose file!\n");
-    cexitt();
+    *ierr=1;
+    return;
   }
 
   fp=NULL;
+  *ierr=0;
 }
 
 void byte_rewind()
@@ -146,7 +172,7 @@ void byte_rewind()
 }
 
 
-void byte_write(float *buf, int *n)
+void byte_write(float *buf, int *n,int *ierr)
 {
   int flags;
   mode_t mode;
@@ -154,7 +180,8 @@ void byte_write(float *buf, int *n)
   if (*n<0)
   {
     printf("byte_write() :: n must be positive\n"); 
-    cexitt();
+    *ierr=1;
+    return;
   }
 
   if (!fp)
@@ -162,7 +189,8 @@ void byte_write(float *buf, int *n)
     if (!(fp=fopen(name,"wb")))
     {
       printf("byte_write() :: fopen failure!\n"); 
-      cexitt();
+      *ierr=1;
+      return;
     }
     flag=WRITE;
   }
@@ -170,31 +198,35 @@ void byte_write(float *buf, int *n)
   if (flag==WRITE)
     {
       if (bytesw_write == 1)
-        byte_reverse (buf,n);
+        byte_reverse (buf,n,ierr);
       fwrite(buf,sizeof(float),*n,fp);
     }
   else
   {
       printf("byte_write() :: can't fwrite after freading!\n"); 
-      cexitt();
+      *ierr=1;
+      return;
   }
+  *ierr=0;
 }
 
 
-void byte_read(float *buf, int *n)
+void byte_read(float *buf, int *n,int *ierr)
 {
   int flags;
   mode_t mode;
 
   if (*n<0)
-    {printf("byte_read() :: n must be positive\n"); cexitt();}
+    {printf("byte_read() :: n must be positive\n"); *ierr=1; return;}
 
   if (!fp)
   {
      if (!(fp=fopen(name,"rb")))
      {
-        printf("filename: %s\n",name);
-        printf("byte_read() :: fopen failure2!\n"); cexitt();
+        printf("%s\n",name);
+        printf("byte_read() :: fopen failure2!\n"); 
+        *ierr=1;
+        return;
      }
      flag=READ;
   }
@@ -202,25 +234,29 @@ void byte_read(float *buf, int *n)
   if (flag==READ)
   {
      if (bytesw_read == 1)
-        byte_reverse (buf,n);
+        byte_reverse (buf,n,ierr);
      fread(buf,sizeof(float),*n,fp);
      if (ferror(fp))
      {
        printf("ABORT: Error reading %s\n",name);
-       cexitt();
+       *ierr=1;
+       return;
      }
      else if (feof(fp))
      {
        printf("ABORT: EOF found while reading %s\n",name);
-       cexitt();
+       *ierr=1;
+       return;
      }
 
   }
   else
   {
      printf("byte_read() :: can't fread after fwriting!\n"); 
-     cexitt();
+     *ierr=1;
+     return;
   }
+  *ierr=0;
 }
 
 void set_bytesw_write (int *pa)
