749829e27254d1dd801091e1a4e657c6f9e53243
b216449 <b216449@df63514b-fc50-0410-8a93-94e60f5a2dad>
2014-10-28 14:20:46 +0000

    Fixed local part of new gs. Still testing. Added in print statements; will be removed when fixed.
    git-svn-id: https://svn.mcs.anl.gov/repos/NEKCEM/trunk@1728 df63514b-fc50-0410-8a93-94e60f5a2dad

--- src/jl/gs_acc.c
+++ src/jl/gs_acc.c
@@ -9,7 +9,7 @@ void fgs_fields_acc(const sint *handle,
   buffer         *buf;
   const unsigned recv = 0^*transpose, send = 1^*transpose;
   uint    i,j,k,bs,uds,dstride,dtrans,vn;
-  int    *l_map,*map;
+  int    *l_map,*map,*t_map;
   char   *bufp,*sbufp,*q;
   double  t,*ud;
   gs_dom  l_dom;
@@ -22,7 +22,8 @@ void fgs_fields_acc(const sint *handle,
   uds = (*stride) * (*n); // Get size of u in number of doubles
 
   // Get the map and other pointers we need.
-  map     = (int*)(fgs_info[*handle]->map_local[1^*transpose]);
+  map     = (int*)(fgs_info[*handle]->map_local[0^*transpose]);
+  t_map   = (int*)(fgs_info[*handle]->map_local[1^*transpose]);
   dstride = *stride;
   l_dom   = fgs_dom[*dom];
   pwd     = fgs_info[*handle]->r.data;
@@ -35,7 +36,11 @@ void fgs_fields_acc(const sint *handle,
   bs = vn*gs_dom_size[l_dom]*(fgs_info[*handle]->r.buffer_size);
   buffer_reserve(buf,bs);
   bufp = buf->ptr;
-
+  //bufp = malloc(bs);
+    //  printf("buflx: %lX\n",(buf->ptr));
+    //  printf("buf: %c\n",*((char*)(buf->ptr)));
+    printf("bufp: %lX\n",bufp);
+    //    printf("bufp: %c\n",*bufp);
   //#pragma acc data create(bufp[0:bs]) present(ud[0:uds])
   {
     // The below implementing cgs_many()/gs_aux():
@@ -51,7 +56,9 @@ void fgs_fields_acc(const sint *handle,
 	  t=ud[i+k*dstride];
 	  j=*l_map++;
 	  do { 
+            printf("gather i: %d j: %d t: %lf out: %lf \n",i,j,t,((double*)q)[j*vn]);
 	    t += ud[j+k*dstride];
+            printf("gather2 i: %d j: %d t: %lf out: %lf \n",i,j,t,((double*)q)[j*vn]);
 	  } while((j=*l_map++)!=-(uint)1);
 	  ud[i+k*dstride]=t;
 	}								
@@ -64,7 +71,7 @@ void fgs_fields_acc(const sint *handle,
       // gs_init_many_acc(u,vn,gsh->flagged_primaries,dom,op);
       {
 	for(k=0;k<vn;++k) {
-	  l_map = map;
+          l_map = fgs_info[*handle]->flagged_primaries;
 	  while((i=*l_map++)!=-(uint)1) {
 	    ud[i+k*dstride]=0.0;
 	  }
@@ -73,20 +80,25 @@ void fgs_fields_acc(const sint *handle,
     }
     /* post receives */
     sbufp = pw_exec_recvs(bufp,vn*gs_dom_size[l_dom],comm,&pwd->comm[recv],pwd->req);
+    printf("bufp: %lX sbu %lX\n",bufp,sbufp);
     /* fill send buffer */
-    // gs_scatter_vec_to_many_acc(sendbuf,data,vn,pwd->map[send],dom);
+    // gs_scatter_many_to_vec_acc(sendbuf,data,vn,pwd->map[send],dom);
     {
       q = sbufp;
       for(k=0;k<vn;k++) {
-	l_map = map;
+	l_map = pwd->map[send];
 	while((i=*l_map++)!=-(uint)1) {				  
 	  t=ud[i+k*dstride];
 	  j=*l_map++;
 	  do {
+            printf("scatter_##T i: %d j: %d t: %lf out: %lf : %d\n",i,j,t,((double*)q)[j*vn]);
 	    ((double*)q)[j*vn]=t;
+            printf("scatter_##T2 i: %d j: %d t: %lf out: %lf : %d\n",i,j,t,((double*)q)[j*vn]);
 	  } while((j=*l_map++)!=-(uint)1); 
-	}								
+	}		
+        printf("p1: %lX\n",q);
 	q+=gs_dom_size[l_dom];
+        printf("p1: %lX\n",q);
       }
     }
     /* post sends */
@@ -97,28 +109,35 @@ void fgs_fields_acc(const sint *handle,
     {
       q = bufp;
       for(k=0;k<vn;k++) {
-	l_map = map;
+	l_map = pwd->map[recv];
 	while((i=*l_map++)!=-(uint)1) { 
 	  t=ud[i+k*dstride];
 	  j=*l_map++; 
 	  do {
+            printf("gather i: %d j: %d t: %lf out: %lf \n",i,j,t,((double*)q)[j*vn]);
 	    t += ((double*)q)[j*vn];
+            printf("gather2 i: %d j: %d t: %lf out: %lf \n",i,j,t,((double*)q)[j*vn]);
 	  } while((j=*l_map++)!=-(uint)1);
 	  ud[i+k*dstride]=t;
-	}                                                                          
+          printf("out: %lf\n",ud[i+k*dstride]);
+	}                               
+        printf("p2: %lX\n",q);
 	q+=gs_dom_size[l_dom];
+        printf("p2: %lX\n",q);
       }
     }
     // --
     // gs_scatter_many_acc(u,u,vn,gsh->map_local[1^transpose],dom); 
     {
       for(k=0;k<vn;++k) {
-	l_map = map;
+	l_map = t_map;
 	while((i=*l_map++)!=-(uint)1) {
 	  t=ud[i+k*dstride];
 	  j=*l_map++; 
 	  do {
+            printf("scatter_##T i: %d j: %d t: %lf out: %lf index: %d\n",i,j,t,ud[j+k*dstride],j+k*dstride);
 	    ud[j+k*dstride]=t; 
+            printf("scatter_##T2 i: %d j: %d t: %lf out: %lf\n",i,j,t,ud[j+k*dstride]);
 	  } while((j=*l_map++)!=-(uint)1);
 	}
       }
