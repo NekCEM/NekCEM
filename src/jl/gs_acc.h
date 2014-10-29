dd8993f8d277b17aa3ef1b6fa62e8be6315f2f47
b216449 <b216449@df63514b-fc50-0410-8a93-94e60f5a2dad>
2014-10-29 17:09:26 +0000

    I added the OpenACC version of gs to use the makefile to compile and link instead of the hack with include. ~Aaron Vose (Cray Inc.)
    git-svn-id: https://svn.mcs.anl.gov/repos/NEKCEM/trunk@1741 df63514b-fc50-0410-8a93-94e60f5a2dad

---
+++ src/jl/gs_acc.h
@@ -0,0 +1,10 @@
+#ifndef GS_ACC_H
+#define GS_ACC_H
+
+#ifndef GS_ACC_C
+extern void fgs_fields_acc(const sint *handle, double *u, const sint *stride, const sint *n,
+			   const sint *dom, const sint *op, const sint *transpose,
+			   struct gs_data **fgs_info);
+#endif
+
+#endif
