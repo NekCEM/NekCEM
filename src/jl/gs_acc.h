9015e688463b91ba5c76b4ec133fed99e54f93d8
Matt Otten <mjo98@cornell.edu>
2014-12-20 19:23:00 +0000

    Added save functionality for gs_acc.c
    git-svn-id: https://svn.mcs.anl.gov/repos/NEKCEM/trunk@1793 df63514b-fc50-0410-8a93-94e60f5a2dad

--- src/jl/gs_acc.h
+++ src/jl/gs_acc.h
@@ -2,6 +2,8 @@
 #define GS_ACC_H
 
 #ifndef GS_ACC_C
+extern void gs_flatmap_setup_acc(const sint *handle, struct gs_data **fgs_info);
+
 extern void fgs_fields_acc(const sint *handle, double *u, const sint *stride, const sint *n,
 			   const sint *dom, const sint *op, const sint *transpose,
 			   struct gs_data **fgs_info);
