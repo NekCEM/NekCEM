f0ff6e69049a650ec49a7615f4b8e1579832c148
misunmin <mmin@mcs.anl.gov>
2015-01-08 17:01:18 +0000

    scale data saved from Matt
    git-svn-id: https://svn.mcs.anl.gov/repos/NEKCEM/trunk@1840 df63514b-fc50-0410-8a93-94e60f5a2dad

---
+++ examples/gpu_scale/data/timingPlot.py
@@ -0,0 +1,71 @@
+import re
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+
+#Run with python timingPlot.py <save>
+#if save is 0, it will save pngs.
+#if save is 1, it will be interactive.
+
+saveIn=sys.argv[1]
+
+for fn in os.listdir('.'):
+    if os.path.isfile(fn):
+        if(re.match('timing.*Sort$',fn)):
+            plt.clf()
+            result=np.loadtxt(fn,dtype=[('No',np.int),('ele',np.int),('time',np.float)])
+            maxNo = max(result['No'])
+            proc=fn[-7:-4]
+            comp=fn[6:-7]
+            i=1
+            while i<=maxNo:
+                plt.plot(result['ele'][result['No'] == i]*15*15*15,result['time'][result['No'] == i], '-o',label=proc+str(i))
+                i=2*i
+
+            plt.legend(loc='upper left')
+            plt.title('Timing Runs for '+comp+proc)
+            plt.ylabel('Time (s)')
+            plt.xlabel('Number of Grid Points')
+            save=fn+'lin'
+            if saveIn == '0':
+                plt.savefig(save+'.png')
+            else:
+                plt.show()
+
+            i=1
+            while i<=maxNo:
+                plt.plot(result['ele'][result['No'] == i]*15*15*15,result['time'][result['No'] == i], '-o',label=proc+str(i))
+                i=2*i
+
+            plt.legend(loc='upper left')
+            plt.title('Timing Runs for '+comp+proc)
+            plt.ylabel('Time (s)')
+
+            plt.xscale('log')            
+            plt.xlabel('log(Number of Grid Points)')
+
+            save=fn+'log'
+            if saveIn == '0':
+                plt.savefig(save+'.png')
+            else:
+                plt.show()
+
+            i=1
+            while i<=maxNo:
+                plt.plot(result['ele'][result['No'] == i]*15*15*15,result['time'][result['No'] == i], '-o',label=proc+str(i))
+                i=2*i
+
+            plt.legend(loc='upper left')
+            plt.title('Timing Runs for '+comp+proc)
+            plt.ylabel('log(Time (s))')
+            
+            plt.yscale('log')
+            plt.xscale('log')          
+            plt.xlabel('log(Number of Grid Points)')
+
+            save=fn+'loglog'
+            if saveIn == '0':
+                plt.savefig(save+'.png')
+            else:
+                plt.show()
