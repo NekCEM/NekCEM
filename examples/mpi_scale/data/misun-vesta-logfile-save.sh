89e8fc59849e25103c03ed37b966b1340d3312ad
misunmin <mmin@mcs.anl.gov>
2015-01-15 02:46:42 +0000

    cleanup
    git-svn-id: https://svn.mcs.anl.gov/repos/NEKCEM/trunk@1926 df63514b-fc50-0410-8a93-94e60f5a2dad

--- examples/mpi_scale/data/misun-vesta-logfile-save.sh
+++ examples/mpi_scale/data/misun-vesta-logfile-save.sh
@@ -3,13 +3,14 @@
 # this is for MPI runs on Vesta/Cetus/Mira
 
 for proc in 'MPI' ; do
-    for nproc in 1 2 4 8 16; do
-	for ele in 1 2 4 8 16 32 64 128; do
+   for nproc in 1 2 4 8 16 32 64 128; do
+        for ele in 1 2 4 8 16 32 64 128 256 512 1024 2048 4096; do
 	    dir=${proc}_${nproc}_${ele}
+
 	    #Make new directory
 	    if [ "$ele" -ge "$nproc" ]; then
 
-	    cp ${dir}/logfile ./mpi_scale/data/smallOutput/vesta/MPI/vesta_${proc}_${nproc}_${ele}
+	    cp ${dir}/logfile ./mpi_scale/data/smallOutput/vesta/MPI/N7/vesta_${proc}_${nproc}_${ele}
             fi
 	done
     done
