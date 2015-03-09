dedb32dc0a7545a928c9ca8c0ae3490679e53cb5
Matt Otten <mjo98@cornell.edu>
2015-03-09 19:44:29 +0000

    Added new plotting script for power consumption.
    git-svn-id: https://svn.mcs.anl.gov/repos/NEKCEM/trunk@1953 df63514b-fc50-0410-8a93-94e60f5a2dad

---
+++ examples/gpu_scale/data/powerPlot.py
@@ -0,0 +1,53 @@
+import re
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+
+#Run with python powerPlot.py <save>
+#if save is 0, it will save pngs.
+#if save is 1, it will be interactive.
+
+saveIn=sys.argv[1]
+saveIn=int(saveIn)
+
+plt.gcf().subplots_adjust(bottom=0.15)
+#!!! Change font here
+plt.rcParams.update({'font.size': 18})
+
+#!!! Change file type here
+fileExtension='.png'
+
+data = np.loadtxt('Cab1-noDate.csv')
+data2 = np.loadtxt('Cab2-noDate.csv')
+#calculate time and then start at 0
+time = data[:,0]*3600 + data[:,1]*60 + data[:,2]
+time = time - time[0]
+#get GPU and CPU times
+gpu = time[9380:9512]
+gpu = gpu - gpu[0]
+cpu = time[9645:9977]
+cpu = cpu -cpu[0]
+#Zero out the outer points to make a prettier plot
+data[9645,3] = 0.0
+data[9380,3] = 0.0
+data2[9645,3] = 0.0
+data2[9380,3] = 0.0
+data[9976,3] = 0.0
+data[9511,3] = 0.0
+data2[9976,3] = 0.0
+data2[9511,3] = 0.0
+#plot data
+plt.plot(cpu,data[9645:9977,3]+data2[9645:9977,3],label='CPU')
+plt.plot(gpu,data[9380:9512,3]+data2[9380:9512,3],label='GPU')
+plt.title('NekCEM Power Consumption')
+plt.xlabel('Time (s)')
+plt.ylabel('Power (kW)')
+plt.legend(loc='lower left')
+plt.ylim(0,30)
+
+save='plots/powerConsumption'
+if saveIn == 0:
+    plt.savefig(save+fileExtension)
+else:
+    plt.show()
