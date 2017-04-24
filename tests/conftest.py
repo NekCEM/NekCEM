22c49b09dba5a51a3955e81059b5e9140f135b28
Josh Wilson <person142@users.noreply.github.com>
2017-04-24 13:37:24 -0500

    Remove dependency on `pytest`
    The tests now rely on the Python standard library only and should
    work with versions of Python between 2.6 and 3.5. This will make
    running tests on Titan/Cetus etc. much easier.

--- tests/conftest.py
+++
@@ -1,8 +0,0 @@
-def pytest_addoption(parser):
-    parser.addoption('--np',
-                     help=('how many processors to run with'))
-    parser.addoption('--clean', action="store_true",
-                     help=('whether to clean before building;'
-                           ' default is False'))
-    parser.addoption('--config', action="store_true",
-                     help=('whether to run nekconfigure'))
